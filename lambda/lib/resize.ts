ec75298fab730316f7c34b6cad56b94dcf64e93b
Wintus <6386129+Wintus@users.noreply.github.com>
2018-07-24 15:27:24 +0900

    Add image resize function

---
+++ lambda/lib/resize.ts
@@ -0,0 +1,37 @@
+import * as sharp from "sharp";
+
+type Data = Buffer | string;
+type Resize = (query: Query) => (data: Data) => Promise<Buffer>;
+
+export interface Query {
+  width?: number;
+  height?: number;
+  webp?: boolean;
+}
+
+export const resize: Resize = ({ width, height, webp }) => async data => {
+  const image = sharp(data);
+  const meta = await image.metadata();
+
+  // guard
+  if (meta.format !== "jpeg") {
+    throw new Error(`file format is not jpeg but: ${meta.format}`);
+  }
+
+  image.rotate(); // before removing metadata
+
+  // resize
+  const w = min(meta.width, width);
+  const h = min(meta.height, height);
+  image.resize(w, h).max(); // keep aspect ratio
+
+  // convert
+  if (webp) {
+    image.webp();
+  }
+
+  return image.toBuffer();
+};
+
+// return null if null
+const min = (defaultNum: number, n?: number) => n && Math.min(defaultNum, n);
