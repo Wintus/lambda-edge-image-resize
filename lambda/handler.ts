8688c29d4fbcd66a88b54e76874e926becf7db3c
Wintus <6386129+Wintus@users.noreply.github.com>
2018-07-24 15:27:35 +0900

    reformat

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,13 +1,21 @@
-import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
 
-export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+import { Query, resize } from "./lib/resize";
+
+// noinspection JSUnusedGlobalSymbols
+export const hello: Handler = (
+  event: APIGatewayEvent,
+  context: Context,
+  cb: Callback,
+) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message:
+        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
       input: event,
     }),
   };
 
   cb(null, response);
-}
+};
