38bf272454c4a2fe055f5c34b93d75976e1ceb75
wintus <yuya0726+GitHub@gmail.com>
2020-03-02 00:24:10 +0900

    Move out inner fns

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -10,18 +10,16 @@ import querystring from "querystring";
 import { isArray } from "util";
 import { Query, resize } from "./lib/resize";
 
+const value = (str?: string | string[]): string =>
+  isArray(str) ? str[0] : str;
+
+const guard = (n?: number): number | null => (isFinite(n) && n > 0 ? n : null);
+
+const parseNum: (str: string | string[]) => number = str =>
+  guard(parseInt(value(str)));
+
 const parseQuery = (queryString: string): Query => {
-  const value = (str?: string | string[]): string =>
-    isArray(str) ? str[0] : str;
-
-  const guard = (n?: number): number | null =>
-    isFinite(n) && n > 0 ? n : null;
-
-  const parseNum: (str: string | string[]) => number = str =>
-    guard(parseInt(value(str)));
-
   const { w, h, webp } = querystring.parse(queryString);
-
   return {
     width: parseNum(w),
     height: parseNum(h),
