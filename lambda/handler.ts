75ab66f6986c17fba4301c422ad3c55b2fd27d22
wintus <yuya0726+GitHub@gmail.com>
2020-03-06 02:02:53 +0900

    Fix by ESLint

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,31 +1,31 @@
 import {
   CloudFrontResponseHandler,
-  CloudFrontResultResponse,
-} from "aws-lambda";
-import { S3 } from "aws-sdk";
-import qs from "querystring";
-import { isArray } from "util";
-import { Query, resize } from "./lib/resize";
+  CloudFrontResultResponse
+} from 'aws-lambda'
+import { S3 } from 'aws-sdk'
+import qs from 'querystring'
+import { isArray } from 'util'
+import { Query, resize } from './lib/resize'
 
 const value = (str?: string | string[]): string =>
-  isArray(str) ? str[0] : str;
+  isArray(str) ? str[0] : str
 
-const guard = (n?: number): number | null => (isFinite(n) && n > 0 ? n : null);
+const guard = (n?: number): number | null => (isFinite(n) && n > 0 ? n : null)
 
 const parseNum: (str: string | string[]) => number = str =>
-  guard(parseInt(value(str)));
+  guard(parseInt(value(str)))
 
 const parseQuery = (queryString: string): Query => {
-  const { w, h, webp } = qs.parse(queryString);
+  const { w, h, webp } = qs.parse(queryString)
   return {
     width: parseNum(w),
     height: parseNum(h),
     webp: Boolean(webp),
-  };
-};
+  }
+}
 
-type S3Object = S3.GetObjectOutput;
-const s3 = new S3();
+type S3Object = S3.GetObjectOutput
+const s3 = new S3()
 
 // destructive
 const resizeS3Image = async <T extends CloudFrontResultResponse>({
@@ -33,36 +33,36 @@ const resizeS3Image = async <T extends CloudFrontResultResponse>({
   query,
   result,
 }: {
-  s3Object: Promise<S3Object>;
-  query: Query;
-  result: T;
+  s3Object: Promise<S3Object>
+  query: Query
+  result: T
 }): Promise<T> => {
   try {
     const buffer = await s3Object
       .then(data => data.Body)
       .then(Buffer.from)
-      .then(resize(query));
+      .then(resize(query))
     // response resized image
-    const encoding = "base64";
-    result.body = buffer.toString(encoding);
-    result.bodyEncoding = encoding;
+    const encoding = 'base64'
+    result.body = buffer.toString(encoding)
+    result.bodyEncoding = encoding
     if (query.webp) {
-      result.headers["content-type"] = [
-        { key: "Content-Type", value: "image/webp" },
-      ];
+      result.headers['content-type'] = [
+        { key: 'Content-Type', value: 'image/webp' },
+      ]
     }
-    return result;
+    return result
   } catch (e) {
-    console.error(e);
+    console.error(e)
     // response any error
-    result.status = "403";
-    result.headers["content-type"] = [
-      { key: "Content-Type", value: "text/plain" },
-    ];
-    result.body = e.toString();
-    return result;
+    result.status = '403'
+    result.headers['content-type'] = [
+      { key: 'Content-Type', value: 'text/plain' },
+    ]
+    result.body = e.toString()
+    return result
   }
-};
+}
 
 // noinspection JSUnusedGlobalSymbols
 export const originResponse: CloudFrontResponseHandler = async ({
@@ -75,57 +75,57 @@ export const originResponse: CloudFrontResponseHandler = async ({
     },
   ],
 }) => {
-  const result = response as CloudFrontResultResponse;
+  const result = response as CloudFrontResultResponse
 
   // guard: check extension
   if (!uri.match(/\.jpe?g$/)) {
     // response original
-    return response;
+    return response
   }
   // guard: check resize
   if (!querystring) {
     // response original
-    return response;
+    return response
   }
   // guard: origin status
   switch (response.status) {
-    case "404":
+    case '404':
       // response not found
-      result.status = "404";
-      result.headers["content-type"] = [
-        { key: "Content-Type", value: "text/plain" },
-      ];
-      result.body = `${uri} is not found.`;
-      return result;
-    case "304":
+      result.status = '404'
+      result.headers['content-type'] = [
+        { key: 'Content-Type', value: 'text/plain' },
+      ]
+      result.body = `${uri} is not found.`
+      return result
+    case '304':
     default:
       // response original
-      return response;
-    case "200":
+      return response
+    case '200':
       // keep going
-      break;
+      break
   }
 
-  const query = parseQuery(querystring);
-  console.debug({ query });
+  const query = parseQuery(querystring)
+  console.debug({ query })
 
   const {
     host: [{ value: hostname }],
-  } = headers;
+  } = headers
   // guard s3 domain
-  const domainRegex = /\.s3\.amazonaws\.com$/;
+  const domainRegex = /\.s3\.amazonaws\.com$/
   if (!hostname.match(domainRegex)) {
-    throw new Error(`invalid S3 hostname: ${hostname}`);
+    throw new Error(`invalid S3 hostname: ${hostname}`)
   }
-  const bucket = hostname.replace(domainRegex, "");
-  const key = uri.slice(1); // remove first `/`
-  console.log("S3 URI:", `s3://${bucket}${uri}`);
+  const bucket = hostname.replace(domainRegex, '')
+  const key = uri.slice(1) // remove first `/`
+  console.log('S3 URI:', `s3://${bucket}${uri}`)
 
   const s3Object = s3
     .getObject({
       Bucket: bucket,
       Key: key,
     })
-    .promise();
-  return await resizeS3Image({ s3Object, query, result });
-};
+    .promise()
+  return await resizeS3Image({ s3Object, query, result })
+}
--- lambda/lib/resize.ts
+++ lambda/lib/resize.ts
@@ -1,38 +1,39 @@
-import sharp from "sharp";
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+import sharp from 'sharp'
 
-type Data = Exclude<Parameters<typeof sharp>[0], sharp.SharpOptions>;
+type Data = Exclude<Parameters<typeof sharp>[0], sharp.SharpOptions>
 export type Query = {
-  width?: number;
-  height?: number;
-  webp?: boolean;
-};
+  width?: number
+  height?: number
+  webp?: boolean
+}
 
 // return null if null
 const min = (defaultNum: number, n?: number): number | null =>
-  n && Math.min(defaultNum, n);
+  n && Math.min(defaultNum, n)
 
 export const resize = (query: Query) => async (data: Data): Promise<Buffer> => {
-  const image = sharp(data);
-  const meta = await image.metadata();
+  const image = sharp(data)
+  const meta = await image.metadata()
 
   // guard
-  if (meta.format !== "jpeg") {
-    throw new Error(`file format is not jpeg but: ${meta.format}`);
+  if (meta.format !== 'jpeg') {
+    throw new Error(`file format is not jpeg but: ${meta.format}`)
   }
 
-  image.rotate(); // before removing metadata
+  image.rotate() // before removing metadata
 
-  const { width, height, webp } = query;
+  const { width, height, webp } = query
 
   // resize
-  const w = min(meta.width, width);
-  const h = min(meta.height, height);
-  image.resize(w, h).max(); // keep aspect ratio
+  const w = min(meta.width, width)
+  const h = min(meta.height, height)
+  image.resize(w, h).max() // keep aspect ratio
 
   // convert
   if (webp) {
-    image.webp();
+    image.webp()
   }
 
-  return await image.toBuffer();
-};
+  return await image.toBuffer()
+}
