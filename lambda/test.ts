d6dfc90d5c9969db2225b3d470a11b85861bb450
wintus <yuya0726+GitHub@gmail.com>
2020-03-09 01:19:52 +0900

    Refactor file promise

--- lambda/test.ts
+++ lambda/test.ts
@@ -2,24 +2,22 @@ import test from 'ava'
 import { promises as fs } from 'fs'
 import { resize } from './lib/resize'
 
-const imagePath = './__tests__/fixtures/original.jpg'
-const resizedImagePath = './__tests__/fixtures/resized.jpg'
+const originalFile = fs.readFile('./__tests__/fixtures/original.jpg')
+const resizedFile = fs.readFile('./__tests__/fixtures/resized.jpg')
 
 test('image buffer read', async t => {
-  const buffer = await fs.readFile(imagePath)
+  const buffer = await originalFile
   t.true(buffer instanceof Buffer)
 })
 
 test('resize image to width=200', async t => {
-  const buffer = await fs.readFile(imagePath)
+  const buffer = await originalFile
   const resized = await resize({ width: 200 })(buffer)
   t.true(resized instanceof Buffer)
 })
 
 test('resized image matches fixture', async t => {
-  const buffer = await fs.readFile(imagePath)
+  const buffer = await originalFile
   const resized = await resize({ width: 200 })(buffer)
-
-  const fixture = await fs.readFile(resizedImagePath)
-  t.deepEqual(resized, fixture)
+  t.deepEqual(resized, await resizedFile)
 })
