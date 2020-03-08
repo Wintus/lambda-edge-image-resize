39e643614a62618f513818e706a7af30000c1b8b
wintus <yuya0726+GitHub@gmail.com>
2020-03-09 01:02:13 +0900

    Add test with image fixture

--- lambda/test.ts
+++ lambda/test.ts
@@ -3,6 +3,7 @@ import { promises as fs } from 'fs'
 import { resize } from './lib/resize'
 
 const imagePath = './__tests__/fixtures/original.jpg'
+const resizedImagePath = './__tests__/fixtures/resized.jpg'
 
 test('image buffer read', async t => {
   const buffer = await fs.readFile(imagePath)
@@ -13,4 +14,12 @@ test('resize image to width=200', async t => {
   const buffer = await fs.readFile(imagePath)
   const resized = await resize({ width: 200 })(buffer)
   t.true(resized instanceof Buffer)
+})
+
+test('resized image matches fixture', async t => {
+  const buffer = await fs.readFile(imagePath)
+  const resized = await resize({ width: 200 })(buffer)
+
+  const fixture = await fs.readFile(resizedImagePath)
+  t.deepEqual(resized, fixture)
 })
