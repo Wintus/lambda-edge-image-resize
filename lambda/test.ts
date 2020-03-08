ec6373cfdaa087f895a6f7eeef7a61a61d7fd721
wintus <yuya0726+GitHub@gmail.com>
2020-03-09 00:53:50 +0900

    Add smoke test of resize

--- lambda/test.ts
+++ lambda/test.ts
@@ -1,9 +1,16 @@
 import test from 'ava'
 import { promises as fs } from 'fs'
+import { resize } from './lib/resize'
 
 const imagePath = './__tests__/fixtures/original.jpg'
 
 test('image buffer read', async t => {
   const buffer = await fs.readFile(imagePath)
   t.true(buffer instanceof Buffer)
+})
+
+test('resize image to width=200', async t => {
+  const buffer = await fs.readFile(imagePath)
+  const resized = await resize({ width: 200 })(buffer)
+  t.true(resized instanceof Buffer)
 })
